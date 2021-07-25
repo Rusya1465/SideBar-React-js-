1714965a0743efb2805946e9e10aa185972a2679
Rustam <rustamotorbaev@gmail.com>
2021-07-25 22:34:45 +0600

    final sidebarjs-v1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import Home from "./page/Home";
+import Products from "./page/Products";
+import Reports from "./page/Reports";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
         </Switch>
       </Router>
     </>

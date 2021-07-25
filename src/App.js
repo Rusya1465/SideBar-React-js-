46792640d81c2dee2ded5f17fde7ac067669b362
Rustam <rustamotorbaev@gmail.com>
2021-07-25 22:06:06 +0600

    add icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
